ff85bccb528ed3af6e55ebabcbb8a6ae458be8eb
Bluebeast29 <130594788+Bluebeast29@users.noreply.github.com>
2024-09-17 18:59:13 +0000

    Conversion Of Datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,29 @@
+let score = false
+
+//      console.log(typeof score);
+//      console.log(typeof(score));
+
+let valueInNumber = Number(score)
+
+//      console.log(typeof valueInNumber);
+//      console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//      console.log(typeof booleanIsLoggedIn);
+     console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "krishna" => true
+
+let number = 23
+let stringNumber = String(number)
+
+//      console.log(stringNumber);
+//      console.log(typeof stringNumber);

0ea00dbaaa10417d6164ea04bdd3fa913927daef
Bluebeast29 <130594788+Bluebeast29@users.noreply.github.com>
2024-09-17 18:18:21 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict"; // Treat all JS code as newer version
+
+// alert("Hello world"); // we arw using noejs, not browser
+
+let name = "Krishna"
+let age = 19
+let isLoggedIn = false
+let state
+
+// number => 2^53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+// object
+
+console.log(typeof null);
+console.log(typeof undefined);

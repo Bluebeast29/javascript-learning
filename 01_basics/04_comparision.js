429f36a520ddec804f71061ff8ec8b0e20252e62
Bluebeast29 <130594788+Bluebeast29@users.noreply.github.com>
2024-09-17 19:49:29 +0000

    Comaparisions basics

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,16 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+// Never do such comps
+
+console.log(2 == "2");
+console.log(2 === "2");

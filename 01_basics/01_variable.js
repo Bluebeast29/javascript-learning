19fd2386f7f3ad6fc8b8a339eb6ff9d8ac6a1974
Bluebeast29 <130594788+Bluebeast29@users.noreply.github.com>
2024-09-17 18:20:02 +0000

    variables

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,15 @@
+const accountId = 144254;
+let accountEmail = "1234@google.com";
+var accountPassword = "12345";
+accountCity = "Surat";
+let accountState;
+
+// accountId = 2 // not allowed
+
+accountEmail = "5678@google.com";
+accountPassword = "78910";
+accountCity = "Bharuch";
+
+console.log(accountEmail);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
